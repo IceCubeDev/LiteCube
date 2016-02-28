038d38db79860f7c7fe0253cc6fed2f05ef87d14
Ivan Dortulov <ivandortulov@yahoo.com>
2016-02-28 23:54:18 +0200

    Modified LiteDefines.h documentation header

--- Include/LiteCube/LiteDefines.h
+++ Include/LiteCube/LiteDefines.h
@@ -1,12 +1,13 @@
-/* LiteDefines.h -- Contains common definitions
- *
- * Copyright (C) 2016 Ivan Dortulov
- *
- * This software may be modified and distributed under the terms
- * of the MIT license.  See the LICENSE file for details.
- */
-#ifndef LITE_CUBE_H
-#define LITE_CUBE_H
+///////////////////////////////////////////////////////////////////////////////
+/// LiteDefines.h -- Contains common definitions
+/// 
+///  Copyright (C) 2016 Ivan Dortulov (ivandortulov@yahoo.com)
+/// 
+///  This software may be modified and distributed under the terms
+///  of the MIT license.  See the LICENSE file for details.
+///////////////////////////////////////////////////////////////////////////////
+#ifndef LITEDEFINES_H
+#define LITEDEFINES_H
 
 #ifdef LITECUBE_EXPORTS
 #define LITE_API __declspec(dllexport)
@@ -14,4 +15,4 @@
 #define LITE_API __declspec(dllimport)
 #endif
 
-#endif
+#endif // LITEDEFINES_H
