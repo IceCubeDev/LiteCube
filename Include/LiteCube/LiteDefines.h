21c679d0a8d3d8d301cc88a1ba0e90a1bad97cbd
Ivan Dortulov <ivandortulov@yahoo.com>
2016-02-28 23:26:57 +0200

    Added LiteDefines.h
    The LiteDefines.h header contains definitions that will be shared across
    the code.

---
+++ Include/LiteCube/LiteDefines.h
@@ -0,0 +1,17 @@
+/* LiteDefines.h -- Contains common definitions
+ *
+ * Copyright (C) 2016 Ivan Dortulov
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+#ifndef LITE_CUBE_H
+#define LITE_CUBE_H
+
+#ifdef LITECUBE_EXPORTS
+#define LITE_API __declspec(dllexport)
+#else
+#define LITE_API __declspec(dllimport)
+#endif
+
+#endif
